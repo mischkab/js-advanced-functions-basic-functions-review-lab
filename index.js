af39780e683032f1500b6b70919a52009f9d1675
Mischka Booley <mischka@gearbox.com.au>
2022-04-27 14:57:00 +1000

    commit tree 00d1e37c7690e8c66b89a7070a1a9853325a9012

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function saturdayFun (activity = 'roller-skate') {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective (highlight = "*") {
+  return function (adjective = "special") {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  };
+}
+
+let Calculator = {
+  add : function() {
+    return 1 + 3; 
+  },
+  subtract : function() {
+    return 1 - 3;
+  },
+  multiply : function () {
+    return 1 * 3;
+  },
+  divide : function() {
+    return 10 / 5;
+  }
+};
+
+function actionApplyer (startInt, array) {
+  let a = startInt;
+  for (let i = 0; i < array.length; i++) {
+    a = array[i](a)
+  }
+
+  return a
+}
